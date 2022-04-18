6a6474f9673cc68adae2034a795da416ef7e13b9
ElleOngori <94163979+Elle-Ongori@users.noreply.github.com>
2022-04-18 14:23:22 +0300

    main

--- main.h
+++ main.h
@@ -20,23 +20,18 @@ typedef struct fmt
 
 
 /* functions */
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
 int _printf(const char *format, ...);
 int print_op(const char *format, fmt_t *print_arr, va_list list);
-char *convert_to(char representation[], unsigned int num, int base);
-int function_manager(char c, va_list arg);
-int _switch(char c, va_list arg);
-int print_character(va_list arg);
-int print_sign(va_list arg, int base);
-int print_unsign(va_list arg, int base);
-int print_string(va_list arg);
-int print_ptr(va_list arg);
-int print_rot13(va_list arg);
-int print_rev(va_list arg);
-int print_base16_upper_lower(va_list arg, char *representation);
-int loop_format(va_list arg, const char *string);
-int call_function_manager(char aux, va_list arg);
-int check_percent(int *flag, char aux);
+int ch(va_list character);
+int str(va_list string);
+int _int(va_list integ);
+int _ui(va_list unsign);
+int _oct(va_list octo);
+int _rot13(va_list rot);
+int _hex_str(unsigned int n, unsigned int hex, char alpha);
+int _hex_l(va_list hexa);
+int _hex_u(va_list hexa);
+int _strlen(char *s);
+int _bin(va_list bin);
+int _putchar(char c);
 #endif
